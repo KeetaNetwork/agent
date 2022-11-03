075167af8f16d2cf986cd3bf6a345e424dad60e0
David Scheutz <dscheutz@keeta.com>
2022-11-03 14:50:58 -0700

    Initial Commit

---
+++ keeta-secretive/keeta_secretiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  keeta_secretiveApp.swift
+//  keeta-secretive
+//
+//  Created by David Scheutz on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct keeta_secretiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
