7dab8d04977ef0ca2d52b49ffc7d6936e3fcd0a4
Ty Schenk <tschenk@keeta.com>
2022-11-07 22:55:46 -0800

    add starter ui

--- Agent/ContentView.swift
+++ Agent/ContentView.swift
@@ -9,13 +9,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Dashboard")
+                    .foregroundColor(KeetaColor.yellow)
+                    .font(.title)
+                Spacer()
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Image("github")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                        Text(verbatim: "Connect with Github")
+                    }
+                }
+                .frame(width: 226, height: 56)
+                .buttonStyle(.plain)
+                .background(KeetaColor.black)
+                .foregroundColor(KeetaColor.yellow)
+                .cornerRadius(40)
+            }
+            VStack(alignment: .leading, spacing: 0) {
+                HStack {
+                    HStack {
+                        Image("Key")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                        Text("GPG Key")
+                            .foregroundColor(KeetaColor.yellow)
+                    }
+                    Spacer()
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            HStack {
+                                Image("Copy")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 20, height: 20)
+                                Text(verbatim: "Copy")
+                            }
+                        }
+                        .buttonStyle(.plain)
+                        .foregroundColor(KeetaColor.red)
+                    }
+                }
+                .padding(EdgeInsets(top: 24.0, leading: 24.0, bottom: 24.0, trailing: 24.0))
+                Spacer()
+                    .frame(width: 552, height: 1)
+                    .background(KeetaColor.gray)
+                Text("""
+                    -----BEGIN PGP PUBLIC KEY BLOCK-----
+                     mFIEY2nI9BMIKoZIzj0DAQcCAwTxpDGIWjacNeBe
+                     vclHJNIvgOaUZ7+ioWivxTLa 5zIyBfxb4NOkyay3ouaV
+                     Cl8HoUcC4OHXLjiy/1itEkPoMfwtBxSb3kgS2VlbmUg
+                     PHJrZWVuZUBrZWV0YS5jb20+iJMEExMIADsWIQQ
+                     gr6QNn6PTC6UWD7YDSS4BiK3SuQUCY2nI9AIbAw
+                     ULCQgHAgMiAgEGFQoJCAsCAxYCAQIeBwIXgAAK
+                     CRADSS4BiK3SuQlwAQDP3MPbKyNXvmyNOvUr3sM
+                     tnN6FXaAh+DhHQkhdYXLDwEAm1STmy7xI/IV
+                    xdkorW16P9q6iHReGNxHX+kl50ZSDVw= =v/u5
+                    -----END PGP PUBLIC KEY BLOCK-----
+                """)
+                .fixedSize(horizontal: false, vertical: true)
+                .foregroundColor(KeetaColor.yellow)
+                .multilineTextAlignment(.leading)
+                .padding(EdgeInsets(top: 0, leading: 24, bottom: 24, trailing: 24))
+            }
+            .background(KeetaColor.black)
+            .cornerRadius(16)
+            Image("Powered")
+                .resizable()
+                .frame(width: 113, height: 24)
+                .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
         }
-        .padding()
+        .padding(EdgeInsets(top: 24, leading: 24, bottom: 24, trailing: 24))
+        .background(KeetaColor.gray)
+        .frame(minWidth: 700, minHeight: 368)
     }
 }
 
---
+++ Agent/Helpers/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Agent
+//
+//  Created by Ty Schenk on 11/7/22.
+//
+
+import SwiftUI
+
+struct KeetaColor {
+    static let black = Color("KeetaBlack")
+    static let gray = Color("KeetaGray")
+    static let yellow = Color("KeetaYellow")
+    static let red = Color("KeetaRed")
+}
