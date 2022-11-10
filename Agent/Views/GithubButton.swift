39953c63d58b082115791d34c779e7dd06b8ec35
Ty Schenk <tschenk@keeta.com>
2022-11-09 23:11:58 -0800

    cleanup github button

--- Agent/Views/GithubButton.swift
+++ Agent/Views/GithubButton.swift
@@ -13,42 +13,38 @@ struct GithubButton: View {
     @ObservedObject var storage: Storage = .shared
     
     var body: some View {
-        if let user = storage.user {
-            HStack {
-                AsyncImage(url: URL(string: user.github.avatarUrl)) { image in
-                    image.resizable()
-                } placeholder: {
-                    KeetaColor.black
-                }
-                .scaledToFit()
-                .frame(width: 20, height: 20)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8))
-                Text(user.github.username)
-            }.padding(8)
-            .background(KeetaColor.black)
-            .foregroundColor(KeetaColor.yellow)
-            .cornerRadius(18)
-        } else {
-            Button {
+        Button {
+            if storage.user == nil {
                 guard let githubUrl = URL(string: "https://agent.keeta.com/api/github/oauth/login?scopes=write:gpg_key&redirectUrl=https://agent.keeta.com/api/github/oauth/callback") else { return }
                 openURL(githubUrl)
-            } label: {
-                HStack {
+            }
+        } label: {
+            HStack {
+                if let user = storage.user {
+                    AsyncImage(url: URL(string: user.github.avatarUrl)) { image in
+                        image.resizable()
+                    } placeholder: {
+                        KeetaColor.black
+                    }
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8))
+                    Text(user.github.username)
+                } else {
                     Image("github")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 20, height: 20)
                         .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8))
-                    Text(verbatim: "Connect with Github")
+                    Text("Connect with Github")
                 }
             }
-            .padding(8)
+        }.padding(10)
             .buttonStyle(.plain)
             .background(KeetaColor.black)
             .foregroundColor(KeetaColor.yellow)
             .cornerRadius(18)
-        }
     }
 }
 
