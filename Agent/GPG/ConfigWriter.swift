ccd8221b500f02cff24d80faa6e8ab65f8a8b774
David Scheutz <dscheutz@keeta.com>
2022-11-22 18:02:54 -0500

    update write system configs

--- Agent/GPG/ConfigWriter.swift
+++ Agent/GPG/ConfigWriter.swift
@@ -6,12 +6,19 @@ final class ConfigWriter {
     private static let newLine = "\n"
     
     static func add(_ config: Config) throws {
+        print("** Attempt to write config \(config)")
+        
         let filePath = configPath + "/\(config.filename)"
         
         let text = config.payload + newLine
         
-        guard fileManager.fileExists(atPath: filePath) && config.isSystem else {
+        let writeNewFile: () -> Void = {
+            print("** Write new config \(config)")
             fileManager.createFile(atPath: filePath, contents: text.data(using: .utf8)!)
+        }
+        
+        guard fileManager.fileExists(atPath: filePath) && config.isSystem else {
+            writeNewFile()
             return
         }
         
@@ -20,9 +27,16 @@ final class ConfigWriter {
         let existing = try handle.readToEnd() ?? .init()
         let existingString = String(data: existing, encoding: .utf8) ?? ""
         
+        guard !existingString.isEmpty else {
+            writeNewFile()
+            return
+        }
+        
         guard !existingString.contains(config.payload) else { return }
         
         try handle.seekToEnd()
+        
+        print("** Append config \(config)")
         
         let data = (existingString.hasSuffix(newLine) ? text : "\(newLine)\(text)").data(using: .utf8)!
         try handle.write(contentsOf: data)
