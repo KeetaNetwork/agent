b432a2138350265499c39ff8536c480ffe9b4876
David Scheutz <dscheutz@keeta.com>
2023-10-13 15:54:52 +0400

    update Storage to use serial number as prefix for keys

--- Agent/Dependencies.swift
+++ Agent/Dependencies.swift
@@ -5,7 +5,7 @@ class Dependencies {
     
     private(set) lazy var keetaAgent = KeetaAgent(secureEnlave: secureEnlave, storage: storage)
     private(set) lazy var secureEnlave = SecureEnclaveStore()
-    private(set) lazy var storage = Storage()
+    private(set) lazy var storage = Storage(prefix: serialNumber())
     
     func setup() {
         Task {
